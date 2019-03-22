b31f27f0caa4634fc864220d81f78304abfd1eec
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2019-03-22 16:54:22 +0100

    smooth scrolling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import SmoothScroll from 'smooth-scroll';
 
 import attachTogglers from './attachTogglers';
 import App from './App';
@@ -7,7 +8,12 @@ import './style/index.scss';
 
 attachTogglers();
 
+new SmoothScroll('a[href*="#"]', { // eslint-disable-line no-new, library API
+    speed: 1000,
+    easing: 'easeInOutCubic',
+});
+
 render(
     <App />,
-    document.getElementById('login-form'),
+    document.getElementById('register'),
 );
