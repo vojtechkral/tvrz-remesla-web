548f155a8167da82c4964090d6cbaae54c7f60fb
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2019-03-23 09:00:39 +0100

    playing around with scroll

---
+++ src/attachScrollListener.js
@@ -0,0 +1,35 @@
+const getTop = (target) => {
+    let element = target;
+    let result = 0;
+    while (element.offsetParent) {
+        result += element.offsetTop;
+        element = element.offsetParent;
+    }
+    return result;
+};
+
+export default () => window.addEventListener('load', () => {
+    const navbar = document.getElementById('mainNav');
+    const title = document.getElementById('title');
+
+    navbar.classList.remove('navbar-shrink');
+
+    document.addEventListener('scroll', () => {
+        const maxScroll = (getTop(title) - navbar.offsetHeight * 1.5);
+        if (window.scrollY > Math.max(maxScroll, 0)) {
+            navbar.classList.add('navbar-shrink');
+        } else {
+            navbar.classList.remove('navbar-shrink');
+        }
+
+        Array.from(document.getElementsByClassName('nav-link')).forEach((navlink) => navlink.classList.remove('active'));
+        Array.from(document.getElementsByTagName('section')).forEach((section) => {
+            const top = getTop(section);
+            const bottom = top + section.offsetHeight;
+            if (window.scrollY > top && window.scrollY < bottom) {
+                const navlink = document.querySelector(`.nav-link[href="#${section.id}"]`);
+                navlink.classList.add('active');
+            }
+        });
+    });
+});
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import {render} from 'react-dom';
 import SmoothScroll from 'smooth-scroll';
 
 import attachTogglers from './attachTogglers';
+import attachScrollListener from './attachScrollListener';
 import App from './App';
 import './style/index.scss';
 
 attachTogglers();
+attachScrollListener();
 
 new SmoothScroll('a[href*="#"]', { // eslint-disable-line no-new, library API
     speed: 1000,
