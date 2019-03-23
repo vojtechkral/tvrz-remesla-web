a72d75c0c832556d2e1815292f9f2855fa8d685b
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2019-03-23 13:37:45 +0100

    more responsivity

--- src/attachTogglers.js
+++ src/attachTogglers.js
@@ -1,8 +1,15 @@
 export default () => {
     const togglers = document.getElementsByClassName('navbar-toggler');
-    Array.from(togglers).forEach((toggler) => toggler.addEventListener('click', () => {
+    Array.from(togglers).forEach((toggler) => {
         const targetId = toggler.dataset.target;
         const target = document.getElementById(targetId);
-        target.classList.toggle('show');
-    }));
+        toggler.addEventListener('click', () => {
+            target.classList.toggle('show');
+        });
+        Array.from(target.getElementsByTagName('a')).forEach((href) => {
+            href.addEventListener('click', () => {
+                target.classList.remove('show');
+            });
+        });
+    });
 };
