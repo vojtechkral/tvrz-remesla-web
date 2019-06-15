64ce764d00eb3d640580cb153e98ef07c63e6aed
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2019-06-15 20:17:36 +0200

    requesty se dají zrušit

--- src/api.js
+++ src/api.js
@@ -1,13 +1,19 @@
 import superagent from 'superagent';
 import * as R from 'ramda';
+import {CANCEL} from 'redux-saga';
+
+const makeCancellable = (request) => {
+    request[CANCEL] = () => request.abort();
+    return request;
+};
 
 const CORS_URL = 'https://cors-anywhere.herokuapp.com/';
 const FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSeXfWlgY2nektgekmEZ1Ju_J95enVR0c_lyBgEmE8rJ_vYHUw/formResponse';
 
-export const submit = (values) => superagent
+export const submit = (values) => makeCancellable(superagent
     .post(`${CORS_URL}${FORM_URL}`)
     .type('form')
-    .send(values);
+    .send(values));
 
 const API_KEY = 'AIzaSyCqgsnrf_ptIMULLJ0bN5IWlWFZQ0VkK84';
 const SPREADSHEET_ID = '1U3NW8vBD63WtNY12KoLj3es0rFrAilalL8t9_YPY0oc';
@@ -23,6 +29,6 @@ const transformFreeSlots = R.pipe(
     R.map(parseInt),
 );
 
-export const getFreeSlots = () => superagent
+export const getFreeSlots = () => makeCancellable(superagent
     .get(GDOC_ULR)
-    .then(transformFreeSlots);
+    .then(transformFreeSlots));
