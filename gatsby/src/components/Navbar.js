323420c0442f1043d50cb25e376408b6c264b62a
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-01-24 18:00:07 +0100

    darkening navbar on scroll

--- gatsby/src/components/Navbar.js
+++ gatsby/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
@@ -7,8 +7,18 @@ import {faBars} from '@fortawesome/free-solid-svg-icons';
 import bootstrap from '../bootstrap.module.scss';
 import style from './Navbar.module.scss';
 
+const isScrolled = () => window.scrollY > 50;
+
 const Navbar = ({title, children}) => {
     const [menuVisible, setMenuVisible] = useState(false);
+    const [scrolled, setScrolled] = useState(isScrolled());
+    useEffect(() => {
+        const onScroll = () => {
+            setScrolled(isScrolled());
+        };
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    }, [setScrolled]);
 
     return (
         <nav
@@ -17,6 +27,7 @@ const Navbar = ({title, children}) => {
                 bootstrap.navbar,
                 bootstrap.navbarExpandLg,
                 bootstrap.fixedTop,
+                {[style.scrolled]: scrolled},
             )}
         >
             <div className={bootstrap.container}>
