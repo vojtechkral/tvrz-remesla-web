fbbe2ae621b3e2f6ff33cc70a4957c4937378eee
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-01-28 11:02:29 +0100

    manifest updated

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -40,12 +40,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        name: 'Řemesla na Tvrzi rosické',
+        short_name: 'Řemesla',
+        start_url: '/',
+        background_color: '#212529',
+        theme_color: '#64a19d',
+        display: 'minimal-ui',
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
