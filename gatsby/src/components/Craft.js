45c49fd2fabfe94e92453a72b3554efd750bd89a
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-01-29 09:34:18 +0100

    clickable images (w/o styling)

--- gatsby/src/components/Craft.js
+++ gatsby/src/components/Craft.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import Img from 'gatsby-image/withIEPolyfill';
+import Img from 'gatsby-image';
 import Carousel, {ModalGateway, Modal} from 'react-images';
 
 import style from './Craft.module.scss';
@@ -13,27 +13,29 @@ const ViewComponent = ({data}) => (
 )
 
 const Craft = ({name, children, images, alternate}) => {
-    const [modalOpen, setModalOpen] = useState(false);
+    const [galleryIndex, setGalleryIndex] = useState(null);
     return (
         <div className={classnames(style.main, {[style.alternate]: alternate})}>
             <div className={style.description}>
                 <h3 className={style.title}>{name}</h3>
                 {children}
-                <button onClick={() => setModalOpen(true)}>Gallery</button>
             </div>
             <div className={style.images}>
-                {images.map(({childImageSharp}) => (
-                    <Img
-                        key={childImageSharp.name}
-                        fluid={childImageSharp.fluid}
-                        className={style.image}
-                    />
+                {images.map(({childImageSharp}, i) => (
+                    <div onClick={() => setGalleryIndex(i)}>
+                        <Img
+                            key={childImageSharp.name}
+                            fluid={childImageSharp.fluid}
+                            className={style.image}
+                        />
+                    </div>
                 ))}
             </div>
             <ModalGateway>
-                {modalOpen && (
-                    <Modal onClose={() => setModalOpen(false)}>
+                {(galleryIndex !== null) && (
+                    <Modal onClose={() => setGalleryIndex(null)}>
                         <Carousel
+                            currentIndex={galleryIndex}
                             components={{View: ViewComponent}}
                             views={images.map(({childImageSharp}) => childImageSharp)}
                         />
