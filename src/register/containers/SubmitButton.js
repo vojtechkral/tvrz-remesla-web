84d686ff10c42dd8a1a18d2bbfa67b2e7371d839
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2022-02-25 10:43:10 +0100

    let user submit incorrect form to learn what is required

--- src/register/containers/SubmitButton.js
+++ src/register/containers/SubmitButton.js
@@ -1,16 +1,11 @@
-import * as R from 'ramda';
 import {connect} from 'react-redux';
-import {isPristine, isInvalid, isSubmitting} from 'redux-form';
+import {isSubmitting} from 'redux-form';
 
 import {SubmitButton} from '../components';
 import {REGISTRATION_FORM} from '../constants';
 
 const mapStateToProps = (state) => ({
-    disabled: R.or(
-        isPristine(REGISTRATION_FORM)(state),
-        isInvalid(REGISTRATION_FORM)(state),
-        isSubmitting(REGISTRATION_FORM)(state),
-    ),
+    disabled: isSubmitting(REGISTRATION_FORM)(state),
 });
 
 const mergeProps = ({disabled}, {onClick}) => ({
